05a2d2093f628404dc5d177507c8db1d92ce0cf8
ahaslett <aaron.haslett@aviatnet.com>
2017-12-20 16:16:34 +1300

    systables commenting and cleanup

--- systables.s
+++ systables.s
@@ -4,23 +4,18 @@
 /*
  * GDT section
  */
+.macro gdt_entry exec_bit priv_bits
+    gdt_\exec_bit\priv_bits://Label so gas will error if we define the same one twice.
+        access = 0x92 | (\exec_bit << 3) | (\priv_bits << 5)
+        .byte 0xff, 0xff, 0x0, 0x0, 0x0, access, 0xcf, 0x0
+.endm
 gdt_start:
-    .long 0
-    .long 0
-gdt_code:
-    .short 0xffff
-    .short 0
-    .byte 0
-    .byte 0x9a
-    .byte 0b11001111
-    .byte 0
-gdt_data:
-    .short 0xffff
-    .short 0
-    .byte 0
-    .byte 0x92
-    .byte 0b11001111
-    .byte 0
+    .long 0, 0
+.irp priv_bits,0,3 //kernel, then user
+    .irp exec_bit,1,0 //Code then data segment.  Do not change order.
+        gdt_entry %exec_bit, %priv_bits
+    .endr
+.endr
 gdt_end_descriptor:
     .short (gdt_end_descriptor - gdt_start) - 1
     .long gdt_start
@@ -32,12 +27,12 @@ idt_start:
     .short idt_end - idt_gates_start - 1
     .long idt_gates_start
 
+//Macro-generation for an IDT entry.  In the initialisation routine at the bottom of this file,
+//this structure will be fixed by moving the higher bits of the handler address reference to the
+//bottom 2 bytes of the IDT entry.  Then, it puts 0x08 (kernel selector) where those bits were.
 .macro idt_entry index
-     .long handler_\index
-    //.short 0x08 //kernel segment selector, to be added.
-    .byte 0 //always 0
-    .byte 0x8e //flags
-    .short 0xcafe  //high offset, to be fixed
+     .long handler_\index//bottom two bits of this will be replaced with kernel selector.
+    .byte 0x0, 0x8e, 0xbe, 0xef//0xBEEF will be replaced with high bits of handler reference.
 .endm
 
 i = 0
@@ -50,30 +45,32 @@ idt_end:
 
 /*
  * IDT handlers.
- * Macro defined to conditionally produce handlers that build up the right stack before calling the common callback that calls the root handler written in C.
+ * Macro defined to produce handlers that build up the right stack before
+ * calling the common callback that calls the root handler written in C.
  */
 .macro handler index ec
-    handler_\index://This is the label that the IDT entries will point to.
+    handler_\index: //This is the label that the IDT entries will point to.
     cli
     pusha
-    .if (\index == 33)//For keyboard, push the scancode
+    .if (\index == 33) //For keyboard, push the scancode
         movl $0x0, %eax
         inb $0x60, %al
         push %eax
     .else
-        .ifb ec//For exceptions without error codes, push 0 to keep stack consistent.
+        .ifb ec //For exceptions without error codes, push 0 to keep stack consistent.
             push $0
         .endif
     .endif
-    push $\index//Push interrupt number.  root_handler will get this.
+    push $\index //Push interrupt number.  root_handler will get this.
     call interrupt_callback
 
-    movb $0x20, %al
-    .if \index >= 40
+    movb $0x20, %al //EOI (End Of Interrupt) code to send to PICs
+    .if \index >= 40 //EOI slave if it produced the interrupt
         outb %al, $0xA0
     .endif
-    outb %al, $0x20
-    add $8, %esp
+    outb %al, $0x20 //Always EOI master because even if slave sent the interrupt, it went through master.
+
+    add $8, %esp //Discard the 2 register pushes we made before calling the callback.
     popa
     sti
     iret
@@ -115,6 +112,7 @@ interrupt_callback:
     movb %al, VID_MEM(,1)
     movb $WoB, 1+VID_MEM(,1)
 
+    //Replace selector with kernel data selector
     mov %ds, %ax
     push %eax
     movw $0x10, %ax
